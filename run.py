f0ef3efd96861e9c370da0e2e66116fa089d5bd2
zhangbo2008 <15122306087@163.com>
2020-03-16 22:32:53 +0800

    1

--- run.py
+++ run.py
@@ -3,6 +3,18 @@ from app import create_app
 app = create_app('app.config')
 # 创建表格、插入数据
 
+
+
+# 初始化的部分都写这里就行了.
+
+model={"初始化":"over"}
+
+
+@app.route('/fun1', methods=['POST'])
+def fun1():
+    return model
+
+
 if __name__ == '__main__':
     app.run(host=app.config['HOST'],
             port=app.config['PORT'],
